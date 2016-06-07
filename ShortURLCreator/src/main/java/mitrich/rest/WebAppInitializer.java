a66fc8601bbae17166b7ad523ed18b3e138e19a8
mitrichtupik <m134@tut.by>
2016-06-07 15:23:23 +0300

    add filter registration

--- ShortURLCreator/src/main/java/mitrich/rest/WebAppInitializer.java
+++ ShortURLCreator/src/main/java/mitrich/rest/WebAppInitializer.java
@@ -1,5 +1,6 @@
 package mitrich.rest;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -8,6 +9,8 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import mitrich.rest.filter.AuthFilter;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
@@ -24,6 +27,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
 		servlet.addMapping("/");
 		servlet.setLoadOnStartup(1);
+
+		FilterRegistration.Dynamic filter = servletContext.addFilter("authFilter", new AuthFilter());
+		filter.addMappingForUrlPatterns(null, false, "/url/", "/urls/user/");
 	}
 
 }
